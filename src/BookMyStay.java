01c8e8a52a0888f3ed57b896e85e35a7b4d56446
Chetna-26 <mohantychetna191@gmail.com>
2026-03-10 14:58:41 +0530

    UC2:Basic Room Types & Static Availability

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,18 +1,69 @@
-ublic class BookMyStay {
+abstract class Room {
+    protected String roomType;
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq.ft");
+        System.out.println("Price per night: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 200, 80);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 120);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 500, 250);
+    }
+}
+
+public class BookMyStay {
+
     public static void main(String[] args) {
 
-        // Printing welcome message
-        System.out.println("=====================================");
-        System.out.println("     Welcome to Book My Stay App     ");
-        System.out.println("=====================================");
+        System.out.println("======================================");
+        System.out.println("Book My Stay - Hotel Booking System");
+        System.out.println("Version: 2.1");
+        System.out.println("======================================");
 
-        // Application information
-        System.out.println("Application: Hotel Booking System");
-        System.out.println("Version: 1.0");
+        Room single = new SingleRoom();
+        Room doub = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        // Informing user that application started
-        System.out.println("System initialized successfully.");
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
 
-        System.out.println("Thank you for using Book My Stay!");
+        System.out.println("\nSingle Room Details:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable);
+
+        System.out.println("\nDouble Room Details:");
+        doub.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable);
+
+        System.out.println("\nSuite Room Details:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
